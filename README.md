79abf4c88c6a76601378ec0e72380d7264b5c69e
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2025-08-18 21:13:23 +0200

    Generate README.md documentation with python scripts;

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Repo analysis 42 evaluation point exchange over time!
+
+![Active users in given date](img/user_over_time_count.png)
+Active user is who still does projects at 42 and is not allumnied, or blackholed
+
+
+
+![Evaluations per day Graph](img/evals_per_day_count.png)
+This data includes every project starting C-Pisciene Common Core and Outter Core, but includes only people who bassed the Piscine
--- create_analysis_and_documentation.py
+++ create_analysis_and_documentation.py
@@ -1,3 +1,39 @@
+import json
+import matplotlib.pyplot as plt
+from datetime import datetime
 
+def basic_plot(data_map, title, x_axis, y_axis, save_name):
+    dates = [d for d, _ in data_map]
+    counts = [c for _, c in data_map]
+    plt.figure(figsize=(10, 5))
+    plt.plot(dates, counts, marker=".")
+    plt.xticks(dates[::30], rotation=30)
+    plt.title(title)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.tight_layout()
+    plt.savefig(f"img/{save_name}", dpi=300)
+
+
+with open("data/perform_temp_analysis.json", "r") as f:
+    data = json.load(f)
+
+
+total_active_users_per_date = data.get("total_active_users_per_date", {})
+total_active_users_per_date_sorted = sorted(
+    total_active_users_per_date.items(),
+    key=lambda x: datetime.fromisoformat(x[0])
+)
+
+basic_plot(data_map=total_active_users_per_date_sorted, x_axis="Date", y_axis="User Count", title="User Count Over Time", save_name="user_over_time_count")
+
+evals_per_day_map = data.get("evals_per_day_map", {})
+
+evals_per_day_map_sorted = sorted(
+    evals_per_day_map.items(),
+    key=lambda x: datetime.fromisoformat(x[0])
+)
+
+basic_plot(data_map=evals_per_day_map_sorted, x_axis="Date", y_axis="Eval Count", title="Evals per day map", save_name="evals_per_day_count")
 
 
