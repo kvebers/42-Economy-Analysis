2455d01856f602dff1dfa83b47e38b0384c6aea8
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2025-08-19 13:53:01 +0200

    Update pictures;

--- create_analysis_and_documentation.py
+++ create_analysis_and_documentation.py
@@ -2,12 +2,18 @@ import json
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-def basic_plot(data_map, title, x_axis, y_axis, save_name):
-    dates = [d for d, _ in data_map]
-    counts = [c for _, c in data_map]
+def basic_plot(data_list, title, x_axis, y_axis, save_name):
+    dates = [d for d, _ in data_list]
+    counts = [c for _, c in data_list]
+    if len(dates) > 1:
+        dates = dates[:-1]
+        counts = counts[:-1]
+    spots = 30
+    if len(counts) != 0:
+        spots = int(len(counts) / 10)
     plt.figure(figsize=(10, 5))
     plt.plot(dates, counts, marker=".")
-    plt.xticks(dates[::30], rotation=30)
+    plt.xticks(dates[::spots], rotation=30)
     plt.title(title)
     plt.xlabel(x_axis)
     plt.ylabel(y_axis)
@@ -29,12 +35,12 @@ basic_plot(data_map=total_active_users_per_date_sorted, x_axis="Date", y_axis="U
 
 evals_per_day_map = data.get("evals_per_day_map", {})
 
-evals_per_day_map_sorted = sorted(
+evals_per_day_list_sorted = sorted(
     evals_per_day_map.items(),
     key=lambda x: datetime.fromisoformat(x[0])
 )
 
-basic_plot(data_map=evals_per_day_map_sorted, x_axis="Date", y_axis="Eval Count", title="Evals per day map", save_name="evals_per_day_count")
+basic_plot(data_map=evals_per_day_list_sorted, x_axis="Date", y_axis="Eval Count", title="Evals per day map", save_name="evals_per_day_count")
 
 
 not_active_points_map = data.get("not_active_points_map", {})
@@ -45,9 +51,9 @@ not_active_points_map_sorted = sorted(
 )
 
 not_active_points_total = 0
-not_active_points_map_total = []
+not_active_points_list_total = []
 for key, value in not_active_points_map_sorted:
     not_active_points_total += value
-    not_active_points_map_total.append((key, not_active_points_total))
+    not_active_points_list_total.append((key, not_active_points_total))
 
-basic_plot(data_map=not_active_points_map_total, x_axis="Date", y_axis="Points", title="Points that are not active in the system", save_name="non_active_points")
+basic_plot(data_map=not_active_points_list_total, x_axis="Date", y_axis="Points", title="Points that are not active in the system", save_name="non_active_points")
